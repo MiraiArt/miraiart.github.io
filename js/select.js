5521ee130ee3864d0edc469c8bb8cd75058ffdd1
Akarimichi <44096526+Akarimichi@users.noreply.github.com>
2019-08-02 22:18:51 +0200

    css maj / select without search

--- js/select.js
+++ js/select.js
@@ -1,5 +1,10 @@
 $(function(){
     $('#select').MA_Select({
+        btnClass: 'bg-pink',
+        inputClass: '',
+        menuClass: 'bg-pink'
+    });
+    $('#selectSearch').MA_Select({
         btnClass: 'bg-blue',
         inputClass: '',
         menuClass: 'bg-blue',
